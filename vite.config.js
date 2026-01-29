d48664f9cebe287f3660bf037518b1866c55b0c5
Adeel Khan <adeelnkhan@gmail.com>
2026-01-28 21:41:25 -0500

    plans via #59

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                plans: resolve(__dirname, 'plans.html'),
+            },
+        },
+    },
+})
